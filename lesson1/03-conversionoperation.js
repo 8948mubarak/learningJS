ed111ee753f4c43d96ccc1cd82d3f1d59c33da40
Mubarak Ansari <107387264+8948mubarak@users.noreply.github.com>
2023-10-05 15:39:06 +0000

    Operators

--- lesson1/03-conversionoperation.js
+++ lesson1/03-conversionoperation.js
@@ -29,3 +29,37 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+// ********************* OPERATIONS ******************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3); 
+*/
+
+let str1 = "hello"
+let str2 = "Mubarak"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("4"+5);
+console.log(4+"5");
+console.log("4"+5+5);
+console.log(4+5+"5");
+
+console.log((3+4)*5%3);   // parenthesis ka use karen 
+
+ let gameCounter = 100
+ gameCounter++
+ console.log(gameCounter)
+
+// study about prefix and postfix from MDN
