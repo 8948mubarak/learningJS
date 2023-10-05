280aa45f8eb4416f57662e2fc5a5ef4026c5cefb
Mubarak Ansari <107387264+8948mubarak@users.noreply.github.com>
2023-10-05 16:08:57 +0000

    Comparision

---
+++ lesson1/04-Comparision.js
@@ -0,0 +1,24 @@
+console.log(3>2); //true
+console.log(3>=2); //true
+console.log(3<2);  //false
+console.log(3==2); //false
+console.log(3!=2); //true
+
+    console.log("3">2);  //true
+    console.log("02">1); //true
+
+console.log(null > 0);   //false       |  is tarah ke comparisions
+console.log(null == 0);  //false       |     ko avoid
+console.log(null >= 0);  //true        |
+                                       
+console.log(undefined == 0); //false   |     Karni hai
+console.log(undefined > 0);  //false   |     Yaad 
+console.log(undefined < 0);  //false   |     Rakhen
+
+// So Comparision & Equality check both are different thing
+
+   //node lesson1/04-Comparision.js
+
+// ===
+
+console.log("5"===5) //false => b'coz conversion k sath hi datatypes v check 
