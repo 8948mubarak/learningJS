5f62109ea6330503292ccaf64298e59eb7a5aec9
Mubarak Ansari <107387264+8948mubarak@users.noreply.github.com>
2023-10-07 11:29:36 +0000

    datatypes-Summary

---
+++ lesson1/datatypes-summary.js
@@ -0,0 +1,47 @@
+// Primitive 
+
+// 7 Types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 90
+const scoreValue = 90.5
+
+const isLoggedIn = false
+const outsideTemp = null //null means Empty not zero
+let useraemail;  // undefined means value is declared but not given
+
+const id = Symbol('987');
+const anotherId = Symbol('987');
+
+console.log(id=== anotherId);
+
+    // node lesson1/datatypes-summary.js
+const bigNumber = 89489979287355n 
+
+
+// Reference ( Non Primitive)
+   
+    // Array, Objects, Functions:>  insabka result function 
+
+  const heros = ["A.P.J.Abdul Kalam", "Bheem Rao", "SubashChandra"] ; //Array short e.g.
+
+  let myObj = {
+    name: "Mubarak",       // Object e.g.
+    age:22,
+  }
+
+  const myFunction = function(){
+    console.log( "Hello G");
+  }
+
+  //console.log(typeof anyvalue)
+
+  /*%%%%  Tyoe of Value & Result type(o/p) %%%%%*/
+
+   //  undefined => "undefined"
+    // Null => "object"
+    // Boolean => "boolean"
+    // Number => "number"
+    // String => "string"
+    // Object => "object"
+    // Object => "function"
+
